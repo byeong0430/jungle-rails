4a23a6ddca8e08860667536be55c9f1a04a95190
byeong0430 <byeong.kim0430@gmail.com>
2018-07-19 05:45:32 +0000

    added admin categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Admin::CategoriesController < ApplicationController
+  def index
+    # get all categories from categories table
+    @categories = Category.order(id: :desc).all
+  end
+
+  def create
+    # execute 'category_params' private function
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to %i[admin categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def new
+    # initiate a 'Category' class
+    @category = Category.new
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+end
