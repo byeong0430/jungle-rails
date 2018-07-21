fcf80d1ca9597a774234f15fdbc80f234e7db7f6
byeong0430 <byeong.kim0430@gmail.com>
2018-07-21 22:05:55 +0000

    created @review instance variable in show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,9 @@ class ProductsController < ApplicationController
   end
 
   def show
+    # make @review available in views/products/new.html.erb
+    @review = Review.new
+
     @product = Product.find params[:id]
     # create an instance class of reviews for the given product id
     # this variable is available in views/products/show.html.erb
