8afc163ca6277300b550e012f136f8317fefbf31
byeong0430 <byeong.kim0430@gmail.com>
2018-07-23 20:39:12 +0000

    changed @product.errors syntax

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,48 +6,48 @@ RSpec.describe Product, type: :model do
     context 'given an empty name field' do
       it 'should not be valid' do
         @product = Product.create
-        expect(@product.errors.full_messages).to include("Name can't be blank")
+        expect(@product.errors[:name]).to include("can't be blank")
       end
     end
 
     context 'given a valid name field' do
       it "should not have 'Name can't be blank' in the error array " do
         @product = Product.create(name: 'name')
-        expect(@product.errors.full_messages).to_not include("Name can't be blank")
+        expect(@product.errors[:name]).to_not include("can't be blank")
       end
     end
 
     it 'should not be valid without a valid price field' do
       @product = Product.create
 
-      expect(@product.errors.full_messages).to include("Price can't be blank")
+      expect(@product.errors[:price]).to include("can't be blank")
     end
     it "should not have 'Price can't be blank' in the error array " do
       @product = Product.create(price: 20)
 
-      expect(@product.errors.full_messages).to_not include("Price can't be blank")
+      expect(@product.errors[:price]).to_not include("can't be blank")
     end
 
     it 'should not be valid without a valid quantity field' do
       @product = Product.create
 
-      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+      expect(@product.errors[:quantity]).to include("can't be blank")
     end
     it "should not have 'Quantity can't be blank' in the error array " do
       @product = Product.create(quantity: 20)
 
-      expect(@product.errors.full_messages).to_not include("Quantity can't be blank")
+      expect(@product.errors[:quantity]).to_not include("can't be blank")
     end
 
     it 'should not be valid without a valid category field' do
       @product = Product.create
 
-      expect(@product.errors.full_messages).to include("Category can't be blank")
+      expect(@product.errors[:category]).to include("can't be blank")
     end
     it "should not have 'Category can't be blank' in the error array " do
-      @product = Product.create(category_id: 1)
-
-      expect(@product.errors.full_messages).to_not include("CATEGORY can't be blank")
+      @category = Category.find(2)
+      @product = @category.products.create
+      expect(@product.errors[:category]).to_not include("can't be blank")
     end
 
   end
