06e29e2f8c144897dc5cf7ca6ca529e0a609cfa4
byeong0430 <byeong.kim0430@gmail.com>
2018-07-24 21:33:06 +0000

    added logic to check if email parameter exists in db

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,17 +11,22 @@ class SessionsController < ApplicationController
     # @user = User.find_by_email(params[:email])
     @user = User.where('lower(email) like ?', params[:email].downcase)[0]
 
-    # email exists in table and password's been authenticated
-    if user = User.authenticate_with_credentials(@user[:email], params[:password])
-      # save user id in cookie session
-      session[:user_id] = user.id
-      session[:expires_at] = Time.current + 24.hours
-      redirect_to :root
+    # check if emeail exists
+    if @user
+      # email exists in table and password's been authenticated
+      if user = User.authenticate_with_credentials(@user[:email], params[:password])
+        # save user id in cookie session
+        session[:user_id] = user.id
+        session[:expires_at] = Time.current + 24.hours
+        redirect_to :root
+      else
+        # password incorrect. send user back to login
+        redirect_to [:new, :session], notice: 'Your email or password is incorrect!'
+      end
     else
-      # login failed. send user back to login
-      redirect_to [:new, :session], notice: 'Your email or password is incorrect!'
+      # email deosn't exist in db. send user back to login
+      redirect_to [:new, :session], notice: 'Your email does not exist!'
     end
-
   end
 
   def destroy
