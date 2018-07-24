aae603cab94f60708da04b007e1a473d73b9c075
byeong0430 <byeong.kim0430@gmail.com>
2018-07-24 00:37:44 +0000

    product spec checks complete

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,54 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  before :each do
+    # create a working example. Then, break it in each validation test
+    def open_asset(file_name)
+      File.open(Rails.root.join('db', 'seed_assets', file_name))
+    end
+    @category = Category.find(2)
+    @product = @category.products.new(
+      name: 'name',
+      description: 'desc',
+      image: open_asset('apparel1.jpg'),
+      price_cents: 1.01,
+      quantity: 1
+    )
+  end
+
   describe 'Validations' do
     # validation tests/examples here
     context 'given an empty name field' do
       it 'should not be valid' do
-        @product = Product.create
+        @product[:name] = nil
+        @product.save
         expect(@product.errors[:name]).to include("can't be blank")
       end
     end
 
-    context 'given a valid name field' do
-      it "should not have 'Name can't be blank' in the error array " do
-        @product = Product.create(name: 'name')
-        expect(@product.errors[:name]).to_not include("can't be blank")
-      end
-    end
-
     it 'should not be valid without a valid price field' do
-      @product = Product.create
-
-      expect(@product.errors[:price]).to include("can't be blank")
-    end
-    it "should not have 'Price can't be blank' in the error array " do
-      @product = Product.create(price: 20)
-
-      expect(@product.errors[:price]).to_not include("can't be blank")
+      @product[:price_cents] = nil
+      @product.save
+      expect(@product.errors[:price_cents]).to include("is not a number")
     end
 
     it 'should not be valid without a valid quantity field' do
-      @product = Product.create
-
+      @product[:quantity] = nil
+      @product.save
       expect(@product.errors[:quantity]).to include("can't be blank")
-    end
-    it "should not have 'Quantity can't be blank' in the error array " do
-      @product = Product.create(quantity: 20)
-
-      expect(@product.errors[:quantity]).to_not include("can't be blank")
     end
 
     it 'should not be valid without a valid category field' do
-      @product = Product.create
-
+      @product[:category_id] = nil
+      @product.save
       expect(@product.errors[:category]).to include("can't be blank")
     end
-    it "should not have 'Category can't be blank' in the error array " do
-      @category = Category.find(2)
-      @product = @category.products.create
-      expect(@product.errors[:category]).to_not include("can't be blank")
-    end
-
   end
 end
